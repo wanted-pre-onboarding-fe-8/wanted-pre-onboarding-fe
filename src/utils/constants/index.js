4239288850b8f879c3c599d308696c196f586e7b
corgi-world <developer.msw@gmail.com>
2022-07-06 10:32:41 +0900

    router 연결

--- src/utils/constants/index.js
+++ src/utils/constants/index.js
@@ -2,4 +2,7 @@ const PUBLIC_PATH = process.env.PUBLIC_URL;
 
 const LOGO_URL = `${PUBLIC_PATH}/logo.png`;
 
-export { PUBLIC_PATH, LOGO_URL };
+const ROUTE_PATH_LOGIN = '/';
+const ROUTE_PATH_FEEDS = '/feeds';
+
+export { PUBLIC_PATH, LOGO_URL, ROUTE_PATH_LOGIN, ROUTE_PATH_FEEDS };
